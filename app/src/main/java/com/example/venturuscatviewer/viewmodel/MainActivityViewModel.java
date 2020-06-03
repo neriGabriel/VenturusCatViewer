4e3546d3f956296aa766434e7bd0e78722ac3df5
Gabriel Neri <neri.gabriel78@gmail.com>
2020-06-03 19:33:44 -0300

    changed to dependecy injection

--- app/src/main/java/com/example/venturuscatviewer/viewmodel/MainActivityViewModel.java
+++ app/src/main/java/com/example/venturuscatviewer/viewmodel/MainActivityViewModel.java
@@ -10,29 +10,36 @@ import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.example.venturuscatviewer.injection.DaggerRetrofitComponent;
 import com.example.venturuscatviewer.model.CatRequest;
 import com.example.venturuscatviewer.model.ImageRequest;
+import com.example.venturuscatviewer.retrofit.DataRequestApi;
 import com.example.venturuscatviewer.retrofit.RetrofitConfig;
 
 import java.util.List;
+
+import javax.inject.Inject;
 
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
 import io.reactivex.rxjava3.schedulers.Schedulers;
 
 public class MainActivityViewModel extends ViewModel {
 
-    private final RetrofitConfig retrofitConfig;
+    //private final RetrofitConfig retrofitConfig;
     private MutableLiveData<List<ImageRequest>> listMutableLiveData = new MutableLiveData<>();
 
+    @Inject
+    public DataRequestApi dataRequestApi;
+
     public MainActivityViewModel() {
-        this.retrofitConfig = new RetrofitConfig();
+        //this.retrofitConfig = new RetrofitConfig();
+        DaggerRetrofitComponent.create().inject(this);
     }
 
     public MutableLiveData<List<ImageRequest>> getApiInfo() {
 
         if(this.listMutableLiveData.getValue() == null) {
-            this.retrofitConfig.getDataRequestApi()
-                               .getApiInfo("cats")
+            this.dataRequestApi.getApiInfo("cats")
                                .subscribeOn(Schedulers.newThread())
                                .observeOn(AndroidSchedulers.mainThread())
                                .subscribe(data -> {
